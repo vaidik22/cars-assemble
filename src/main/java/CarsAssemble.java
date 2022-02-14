45229eed990118887f2f31dc46688f05d78363a1
vaidik22 <vaidik.nigam_cs19@gla.ac.in>
2022-02-14 20:09:43 +0530

    last commit

--- src/main/java/CarsAssemble.java
+++ src/main/java/CarsAssemble.java
@@ -1,10 +1,24 @@
 public class CarsAssemble {
+    private static final int CARS_PRODUCED_EACH_HOUR = 221;
+
+    private double calculateEfficiency(int assemblyLineSpeed) {
+        if (assemblyLineSpeed >= 1 && assemblyLineSpeed <= 4) {
+            return 1.0;
+        } else if (assemblyLineSpeed >= 5 && assemblyLineSpeed <= 8) {
+            return 0.9;
+        } else if (assemblyLineSpeed == 9) {
+            return 0.8;
+        } else if (assemblyLineSpeed == 10) {
+            return 0.77;
+        } else return 0.0;
+    }
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.productionRateperHour() method");
+        return speed * CARS_PRODUCED_EACH_HOUR * calculateEfficiency(speed);
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.workingItemsPerMinute() method");
+        return Double.valueOf(productionRatePerHour(speed) / 60).intValue();
     }
 }
+
